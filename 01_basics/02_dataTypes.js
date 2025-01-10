3085d58f710f702fa6f8bb6ca562df01cb637433
sonusingh <155667520+sonusinghhh@users.noreply.github.com>
2025-01-10 08:25:38 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) // we are going nodejs , not browser
+
+console.log(3 
+    +
+     3) // code readality should be high
+
+     console.log("Hitesh")
+
+     let name = "hitesh"
+     let age = 18
+     let isLoggedIn = false
+     let state;
+
+     //number => 2 to power 53
+     //bigint
+     //string => ""
+     //boolean => true/false
+     //null => standalone value
+     //undefined => 
+    // symbol => unique 
+
+
+     //object 
+     console.log(typeof undefined) // type is unfined
+     console.log(typeof null);   // but it's type is object
