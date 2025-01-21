60c445b0dd3bddafb638186c346cc801b51c148c
sonusingh <155667520+sonusinghhh@users.noreply.github.com>
2025-01-21 09:52:27 +0000

    control flow code files

---
+++ 04_control_flow/one.js
@@ -0,0 +1,50 @@
+//if
+const isUserLoggedIn = true
+const temperature = 41
+
+// if(temperature === 40){
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("tempareture is greater than 50");    
+// }
+
+// console.log("Execute");
+// < , > ,<= , >= , == , != ,=== , !==
+
+// const score = 200
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`user power : ${power}`);
+    
+// }
+//console.log(`user power : ${power}`);
+
+// const balance = 1000
+// if(balance > 500) console.log("test") , console.log("test2");
+// if(balance < 500){
+//     console.log("less than 500");
+    
+// }
+// else if(balance < 900){
+//     console.log("less than 750");
+    
+// }
+// else{
+//     console.log("less than 1200");
+    
+// }
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard && 2==3){
+    console.log("allow ot buy course");
+    
+}
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("user logged in");
+    
+}
+
